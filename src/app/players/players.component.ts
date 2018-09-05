1b8fd577766eb1d90974d2d16432b730f1304e0a
Raj Karthik <rj@chalkstreet.com>
2018-09-05 14:50:03 +0530

    Step 5.2 Condition 1

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { PlayerClass } from '../player';
 import {PlayerService} from '../player.service';
 
@@ -7,7 +7,9 @@ import {PlayerService} from '../player.service';
   templateUrl: './players.component.html',
   styleUrls: ['./players.component.css']
 })
+
 export class PlayersComponent implements OnInit {
+  @Input('firstScreen') firstScreen : any = true;
   constructor(private playerservice : PlayerService) { }
 
   ngOnInit() {
